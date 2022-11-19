3362532f9cf4f87ed86f5f187df768fef6482589
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-11-19 09:38:21 +0300

    added test cases for reading account and if the account not found.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,25 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        new_account = resp.get_json()
+        self.assertEqual(new_account["name"], account.name)
+        self.assertEqual(new_account["email"], account.email)
+        self.assertEqual(new_account["address"], account.address)
+        self.assertEqual(new_account["phone_number"], account.phone_number)
+        self.assertEqual(new_account["date_joined"], str(account.date_joined))\
+
+
+    def test_account_not_found(self):
+        """It should not find an Account"""
+        resp = self.client.get(
+            f"{BASE_URL}/0",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
